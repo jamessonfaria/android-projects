a3df6b59e89db2d3dfac21fa8f33cbf0eea00418
Jamesson Faria <jamessonjr@gmail.com>
2017-04-30 23:18:50 -0300

    Projeto para testes de layouts

---
+++ Ex06_Layouts/app/src/main/java/jamesson/com/br/ex06_layouts/MainActivity.java
@@ -0,0 +1,13 @@
+package jamesson.com.br.ex06_layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex_scroll);
+    }
+}
